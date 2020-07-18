3a0c7e793121249d9985e584f2d70091f56cb106
Deepak Shkaya <deepakshakya4455@gmail.com>
2020-07-19 01:05:50 +0530

    about page start

--- src/containers/About.js
+++ src/containers/About.js
@@ -1,31 +1,60 @@
 import React, { Component } from "react";
 import "./About.css";
-import Navbar from "../components/Navbar";
-import Skill from "../components/Skills";
-//import bg from "../images/bg.jpg";
+import { Link } from "react-router-dom";
+//import Navbar from "../components/Navbar";
+//import Skill from "../components/Skills";
+import img2 from "../images/img2.jpeg";
 class About extends Component {
   render() {
     return (
-      <div>
-        <div className="about">
-          <Navbar />
-          <div className="content">
-            <h1>Deepak Shakya</h1>
-            <p>
-              Bachelor of Technology in Computer Science and Enginnering <br />
-              Integral University,Lucknow
-            </p>
+      <div className="about-grid">
+        <div className="Navbar">
+          <nav className="fill">
             <ul>
-              <li>Specialization in Cloud Computing</li>
-              <li>AWS Solution Architect Associate</li>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <a href="!#">Contacts</a>
+              </li>
+              <li>
+                <Link to="/projects">Projects</Link>
+              </li>
             </ul>
-          </div>
+          </nav>
         </div>
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
+        <div className="grid-item">
+          <h1>
+            <strong>About Me</strong>
+          </h1>
+          <img src={img2} alt="profile-pic" />
+        </div>
+        <div className="grid-item2">
+          <h1 style={{ textAlign: "center" }}>Deepak Shakya</h1>
+          <p>
+            Crud indignant permissively through burped nodded timorous onto the
+            as wore less ouch far rewound considering so broken dachshund
+            jeepers sanely confident. Contemplated growled apart
+            enthusiastically punitively much much darn onto deep dear returned
+            some cockatoo hungrily fortuitously enchantingly ouch sanely on
+            unceremonious especially much yikes darn.
+          </p>
+          <p>
+            Gecko far one before ouch far indistinctly ouch much doubtfully the
+            alas some classically far insincerely much honey close savage ground
+            according enthusiastic and that then about realistic however more
+            forlornly dear demonstrably a this.
+          </p>
+          <p>
+            Less urgently ape one some hamster much well that dolphin strode hey
+            underneath so eagle ouch a hey turtle notwithstanding truly
+            censoriously congratulated dear one mandrill weak much then
+            disconsolately wholesome.
+          </p>
+        </div>
       </div>
     );
   }
