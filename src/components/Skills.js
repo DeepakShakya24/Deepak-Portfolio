c7a44155c19710a0b7d033d9c585b4b54e49421d
Deepak Shkaya <deepakshakya4455@gmail.com>
2020-07-19 02:37:19 +0530

    about page start

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,11 +1,57 @@
 import React, { Component } from "react";
-
+import "./Skills.css";
 class Skill extends Component {
   render() {
     return (
-      <div className="grid-container">
-        <div className="grid-item">
-          <h1 style={{ textAlign: "center" }}>My Skills</h1>
+      <div className="skill-content">
+        <h1 style={{ textAlign: "center" }}>My Skills</h1>
+        <div className="grid-container">
+          <div className="grid-item">
+            <img
+              src="https://ppc.land/wp-content/uploads/2018/03/aws.png"
+              alt="img"
+            />
+          </div>
+          <div className="grid-item">
+            <h2>Amazon Web Services</h2>
+            <p>I have all Knowledge of AWS</p>
+          </div>
+        </div>
+        <div className="grid-container2">
+          <div className="grid-item">
+            <h2>DevOps</h2>
+            <p>I have all Knowledge of AWS</p>
+          </div>
+          <div className="grid-item">
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSKp1zbVbFzJzQiBpEJS1RkEywLBPWWASAOnw&usqp=CAU"
+              alt="img"
+            />
+          </div>
+        </div>
+        <div className="grid-container">
+          <div className="grid-item">
+            <img
+              src="https://icon-icons.com/icons2/2107/PNG/256/file_type_django_icon_130645.png"
+              alt="img"
+            />
+          </div>
+          <div className="grid-item">
+            <h2>Django</h2>
+            <p>I have all Knowledge of AWS</p>
+          </div>
+        </div>
+        <div className="grid-container2">
+          <div className="grid-item">
+            <h2>Full-Stack Developer</h2>
+            <p>I have all Knowledge of AWS</p>
+          </div>
+          <div className="grid-item">
+            <img
+              src="https://img-a.udemycdn.com/course/750x422/822444_a6db.jpg"
+              alt="img"
+            />
+          </div>
         </div>
       </div>
     );
